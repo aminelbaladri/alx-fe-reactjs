781709186b216eb1bb1b430edad15f1f39832a5e
aminelbaladri <aminelbaladri@gmail.com>
2024-08-10 18:24:29 +0100

    new

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,6 +3,9 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import WelcomeMessage from "./components/WelcomeMessage"
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -17,7 +20,11 @@ function App() {
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
+        
       </div>
+      <Header/>
+      <MainContent />
+       <Footer/>
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
@@ -31,6 +38,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
+    
   )
 }
 
