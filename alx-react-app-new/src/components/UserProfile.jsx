e22289dcc5feb773e8bb5a34ecd0e1addd22dffe
aminelbaladri <aminelbaladri@gmail.com>
2024-08-15 20:38:12 +0100

    stylecss

--- alx-react-app-new/src/components/UserProfile.jsx
+++ alx-react-app-new/src/components/UserProfile.jsx
@@ -1,10 +1,10 @@
 function UserProfile(props){
     return (
-      <div style={{border:"1px",padding:"10px",margin:"20px"}}>
-        <h2 style={{color:"green"}}>{props.name}</h2>
-        <p style={{fontSize:"20px"}}>Age: {props.age}</p>
-        <p>Bio: {props.bio}</p>
-      </div>
+      <div style={{ border: '1px solid gray', padding: '10px', margin: '10px' }}>
+   <h2 style={{ color: 'blue' }}>{props.name}</h2>
+   <p>Age: <span style={{ fontWeight: 'bold' }}>{props.age}</span></p>
+   <p>Bio: {props.bio}</p>
+ </div>
     );
   };
   export default UserProfile;
