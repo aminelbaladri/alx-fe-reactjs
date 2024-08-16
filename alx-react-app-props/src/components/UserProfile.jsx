92628787cd639388f5eccaae44624caebb8f20a5
aminelbaladri <aminelbaladri@gmail.com>
2024-08-16 19:18:48 +0100

    update

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,10 +1,19 @@
-function UserProfile(props){
-    return (
+import React, { useContext } from 'react';
+import UserContext from '../UserContext';
+
+import React from 'react';
+
+const UserProfile = () => {
+  const userData = useContext(UserContext);
+
+  return (
       <div>
-        <h2>{props.name}</h2>
-        <p>Age: {props.age}</p>
-        <p>Bio: {props.bio}</p>
+          <h2>User Profile</h2>
+          <p>Name: {userData.name}</p>
+          <p>Email: {userData.email}</p>
       </div>
-    );
-  };
-  export default UserProfile;
+  );
+};
+
+
+export default UserProfile;
