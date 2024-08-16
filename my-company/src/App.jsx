be4d3b4b7aeee8d4ba1af187664f0928f606827e
aminelbaladri <aminelbaladri@gmail.com>
2024-08-16 21:19:14 +0100

    project

---
+++ my-company/src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Home from './Home';
+import About from './About';
+import Services from './Services';
+import Contact from './Contact';
+import Navbar from './Navbar';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+
+
+
+function App() {
+  return (
+    <BrowserRouter>
+    <Navbar/>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='about' element={<About/>}/>
+      <Route path='services' element={<Services/>}/>
+      <Route path='contact' element={<Contact/>}/>
+      <Route path='*' element={<ErrorPage/>}/>
+    </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
