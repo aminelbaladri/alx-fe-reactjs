8ad5880ec8cadeca514f97bc00914e84f42120bb
aminelbaladri <aminelbaladri@gmail.com>
2024-08-24 00:34:13 +0100

    searchbar

---
+++ recipe-sharing-app/src/SearchBar.jsx
@@ -0,0 +1,14 @@
+import useRecipeStore from "./recipeStore";
+
+const SearchBar = () => {
+  const setSearchTerm = useRecipeStore((state) => state.setSearchTerm);
+
+  return (
+    <input
+      type="text"
+      onChange={(e) => setSearchTerm(e.target.value)}
+    />
+  );
+};
+
+export default SearchBar;
